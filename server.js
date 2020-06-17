10e0da4f6349f02a9921ae48bf3c7b77bf051b9d
henry <ihenvyr@gmail.com>
2020-06-17 15:23:39 +0800

    simple scraper demo endpoints added

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const Insta = require("scraper-instagram");
+const InstaClient = new Insta();
+
+app.get("/user/:username", (req, res) => {
+  InstaClient.getProfile(req.params.username)
+    .then(profile => res.json(profile))
+    .catch(error => res.json(error));
+});
+
+app.get("/hashtag/:hashtag", (req, res) => {
+  InstaClient.getHashtag(req.params.hashtag)
+    .then(hashtag => res.json(hashtag))
+    .catch(error => res.json(error));
+});
+
+app.listen(8000, () => console.log(`Server is running on http://localhost:8000`))
