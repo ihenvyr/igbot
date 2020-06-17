be3b1e3188afa781d6a346eaf0a73bb168271829
henry <ihenvyr@gmail.com>
2020-06-17 15:35:26 +0800

    use sessionid

--- server.js
+++ server.js
@@ -1,18 +1,28 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 const Insta = require("scraper-instagram");
 const InstaClient = new Insta();
 
-app.get("/user/:username", (req, res) => {
-  InstaClient.getProfile(req.params.username)
-    .then(profile => res.json(profile))
-    .catch(error => res.json(error));
-});
+async function main() {
+  await InstaClient.authBySessionID(process.env.SESSIONID)
+    // .then(account => console.log(account))
+    .catch(err => console.error(err));
 
-app.get("/hashtag/:hashtag", (req, res) => {
-  InstaClient.getHashtag(req.params.hashtag)
-    .then(hashtag => res.json(hashtag))
-    .catch(error => res.json(error));
-});
+  app.get("/user/:username", (req, res) => {
+    InstaClient.getProfile(req.params.username)
+      .then(profile => res.json(profile))
+      .catch(error => res.json(error));
+  });
 
-app.listen(8200, () => console.log(`Server is running on http://localhost:8200`))
+  app.get("/hashtag/:hashtag", (req, res) => {
+    InstaClient.getHashtag(req.params.hashtag)
+      .then(hashtag => res.json(hashtag))
+      .catch(error => res.json(error));
+  });
+
+  app.listen(8200, () => console.log(`Server is running on http://localhost:8200`));
+}
+
+main();
